5a0616645080f4b7d43c272d510d6f3ffee5f468
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-09-02 14:29:55 +0530

    added some changes

--- temp.py
+++ temp.py
@@ -7,14 +7,16 @@ import time
 
 
 ROUND = 3
-DELTA_TIME = 0.05
+RADIUS = 5
+DELTA_TIME = 0.03
 END = 4
+SCALE = 5
 g = 9.8
 G = 6969
+FONT_SIZE = 17
+FONT = ('Arial', FONT_SIZE, 'bold')
 
 class Projectile_Object:
-	velocity_x = 0
-	velocity_y = 0
 	velocity_0 = 0
 	xcor = 0
 	ycor = 0
@@ -39,14 +41,20 @@ class Projectile_Object:
 	def get_yvel(self, t):
 		return round( (self.velocity_0*math.sin(self.angle)-g*t) , ROUND)
 	
+	def get_flighttime(self):
+		return round( (2*self.velocity_0*math.sin(self.angle)/g) , ROUND)
+	
+	def max_height(self):
+		return round( (((self.velocity_0*math.sin(self.angle))**2)/(2*g)) , ROUND)
+	
 def float_range(start, end, step):
 	while start < end:
 		yield round(start+step, ROUND)
 		start += step
 	
 def take_inputs():
-	velocity = input()
-	angle = input()
+	velocity = input("Enter velocity :")
+	angle = input("Enter angle of launch :")
 	return list([int(velocity), int(angle)])
 	
 def draw_ground(window):
@@ -54,36 +62,66 @@ def draw_ground(window):
 	gr.speed(0)
 	gr.hideturtle()
 	gr.penup()
-	gr.goto(-300, 0)
-	gr.fillcolor("white")
+	gr.goto(-800,-250)
+	gr.fillcolor("#734d26")
 	gr.begin_fill()
 	for _ in range(2):
-		gr.forward(600)
+		gr.forward(1600)
 		gr.right(90)
-		gr.forward(5)
+		gr.forward(500)
 		gr.right(90)
 	gr.end_fill()
 	window.update()		
 
 def launch(t, ball, pen, window):
-	pen.penup()
-	pen.goto(-150,0)
+	trail = turtle.Turtle()
+	trail.hideturtle()
+	trail.speed(0)
+	
+	trail.goto(-505,-250)
+	trail.color("white")
+	trail.left(math.degrees(ball.angle))
+	trail.forward(350)
+	trail.penup()
+	
+	maxheight = ball.max_height()
+	trail.color("red")
+	trail.goto(-500,-250)
+	pen.goto(-500, -250)
 	for _t in t:
 		time.sleep(DELTA_TIME)
 		pen.clear()
+		trail.fillcolor("red")
 		pen.fillcolor("red")
 		pen.begin_fill()
-		pen.circle(5)
+		trail.begin_fill()
+		pen.circle(RADIUS)
+		trail.circle(1)
 		pen.end_fill()
-		pen.goto(ball.get_xcor(_t)*10-150, ball.get_ycor(_t)*10)
-		window.update()
+		trail.end_fill()
 		
-
+		pen.goto(ball.get_xcor(_t)*SCALE-500, ball.get_ycor(_t)*SCALE-250)
+		trail.goto(ball.get_xcor(_t)*SCALE-500, ball.get_ycor(_t)*SCALE-250+RADIUS)
+		
+		trail.penup()
+		window.update()
+	
+def draw_launch(window):
+	ln = turtle.Turtle()
+	ln.hideturtle()
+	ln.speed(0)
+	ln.goto(0, 300)
+	ln.color("white")
+	ln.write("Hit 'Space' to LAUNCH", align="center", font=FONT)
+	window.update()
+	
+	
 
 def main():
 	inputs = take_inputs()
 	ball = Projectile_Object(inputs[0], inputs[1])
 	
+	END = ball.get_flighttime()
 	time_list = list(float_range(0, END, DELTA_TIME))
 	
 	pen = turtle.Turtle()
@@ -93,20 +131,15 @@ def main():
 	window = turtle.Screen()
 	window.title("projectile simulator")
 	window.bgcolor("black")
-	window.setup(width=600, height=500)
+	window.setup(width=1400, height=800)
 	window.tracer(0)
 	
+	draw_launch(window)
 	draw_ground(window)
+	print(ball.max_height())
 	
-	#turtle.onscreenclick(btnclick, 1)
-	
-	
-	# pen.goto(0, 0)
-	# pen.clear()
-	# pen.fillcolor("black")
-	# pen.begin_fill()
-	# pen.circle(50)
-	# pen.end_fill()
+	window.listen()
+	window.onkeyrelease(lambda:launch(time_list, ball, pen, window), "space")
 	
 	window.mainloop()
 
