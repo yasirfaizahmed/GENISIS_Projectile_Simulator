68b1db46efe175d101265724135303900c7ca501
yasirfaizahmed <yasirfaizahmed.n@gmail.com>
2021-09-04 13:55:43 +0530

    all test done

--- test_main.py
+++ test_main.py
@@ -23,3 +23,16 @@ def test_get_yvel():
     assert (13.875 == test_ball.get_yvel(0.3))
     assert (14.625 == test_ball.get_yvel(0.1))    
     
+def test_flighttime():
+    assert (8.0 == test_ball.get_flighttime())
+
+def test_max_height():
+    assert (30.0 == test_ball.max_height())
+    
+def test_range():
+    assert (207.846 == test_ball.range())
+
+test_list = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0]
+def test_float_range():
+    assert (test_list == list(float_range(0, 4, 0.2)))
+       
